11f8fd20b6b716e3a2c5df91669da4d227899df0
Dan <dburnside22@gmail.com>
2019-06-08 13:15:39 -0500

    generated alot of components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { LoginPageComponent } from './login-page/login-page.component';
+import { CalendarViewComponent } from './calendar-view/calendar-view.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingPageComponent,
+    NavigationComponent,
+    LoginPageComponent,
+    CalendarViewComponent
   ],
   imports: [
     BrowserModule,
