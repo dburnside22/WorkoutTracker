6204a36f65837cb821335e5f6b9a94d29dfba24d
Dan <dburnside22@gmail.com>
2019-06-08 13:19:44 -0500

    added first route to app.routing and got ride of junk code

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginPageComponent } from './login-page/login-page.component';
 
 const routes: Routes = [
-  
+    { path: '', component: LoginPageComponent }
 ];
 
 @NgModule({
