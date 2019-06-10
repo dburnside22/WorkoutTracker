d3f99535c131498b28d0c486e893d68c01c571f8
Dan <dburnside22@gmail.com>
2019-06-10 13:29:39 -0500

    added home path to routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginPageComponent } from './login-page/login-page.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
 
 const routes: Routes = [
-    { path: '', component: LoginPageComponent }
+    { path: '', component: LoginPageComponent },
+    { path: 'home', component: LandingPageComponent },
 ];
 
 @NgModule({
