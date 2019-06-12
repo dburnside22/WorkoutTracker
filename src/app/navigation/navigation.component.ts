a111a76beab6b88ce2580a16899d108f850cc9a0
Dan <dburnside22@gmail.com>
2019-06-12 09:12:21 -0500

    added access to the router to get the url in html

--- src/app/navigation/navigation.component.ts
+++ src/app/navigation/navigation.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-navigation',
@@ -9,7 +10,7 @@ export class NavigationComponent implements OnInit {
 
   public navClicked = false;
 
-  constructor() { }
+  constructor(public router: Router) { }
 
   ngOnInit() {
   }
