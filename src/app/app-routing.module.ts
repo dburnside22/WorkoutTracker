fff9b094e0b3422236af0bb04ff2402cc67b8375
Dan <dburnside22@gmail.com>
2019-06-12 09:22:58 -0500

    added register route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,14 @@ import { LoginPageComponent } from './login-page/login-page.component';
 import { LandingPageComponent } from './landing-page/landing-page.component';
 import { HistoryComponent } from './history/history.component';
 import { CalendarViewComponent } from './calendar-view/calendar-view.component';
+import { RegistrationComponent } from './registration/registration.component';
 
 const routes: Routes = [
     { path: '', component: LoginPageComponent },
     { path: 'home', component: LandingPageComponent },
     { path: 'history', component: HistoryComponent },
     { path: 'calendar', component: CalendarViewComponent },
+    { path: 'register', component: RegistrationComponent },
 ];
 
 @NgModule({
