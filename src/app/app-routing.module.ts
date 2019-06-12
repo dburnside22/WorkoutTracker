970cba039ef844212d6822ce07db24668c6e1d55
Dan <dburnside22@gmail.com>
2019-06-12 09:17:52 -0500

    added additional routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginPageComponent } from './login-page/login-page.component';
 import { LandingPageComponent } from './landing-page/landing-page.component';
+import { HistoryComponent } from './history/history.component';
+import { CalendarViewComponent } from './calendar-view/calendar-view.component';
 
 const routes: Routes = [
     { path: '', component: LoginPageComponent },
     { path: 'home', component: LandingPageComponent },
+    { path: 'history', component: HistoryComponent },
+    { path: 'calendar', component: CalendarViewComponent },
 ];
 
 @NgModule({
